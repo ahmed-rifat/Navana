5bda931e1339637b2923034a3aadbb344a6c5a8c
Ahmed Rifat <rifatbafsc@gmail.com>
2022-06-24 01:03:33 +0600

    a demo card added

--- card.js
+++ card.js
@@ -1,20 +1,41 @@
-document.getElementById('layout1btn').addEventListener('click', function(){
+document.getElementById('cardOne').style.display ='block';
+document.getElementById('cardTwo').style.display ='none';
+document.getElementById('cardThree').style.display ='none';
+document.getElementById('layout1btn').style.backgroundColor ='#606060';
+document.getElementById('layout2btn').style.backgroundColor ='#E0E0E0';
+document.getElementById('layout3btn').style.backgroundColor ='#E0E0E0';
+
+
+const lay1=document.getElementById('layout1btn').addEventListener('click', function(){
     document.getElementById('layout1btn').style.backgroundColor ='#606060';
     document.getElementById('layout2btn').style.backgroundColor ='#E0E0E0';
     document.getElementById('layout3btn').style.backgroundColor ='#E0E0E0';
+
+    // card one
+    document.getElementById('cardOne').style.display ='block';
+    document.getElementById('cardTwo').style.display ='none';
+    document.getElementById('cardThree').style.display ='none';
     
   })
 
-  document.getElementById('layout2btn').addEventListener('click', function(){
+   const lay2 =document.getElementById('layout2btn').addEventListener('click', function(){
      document.getElementById('layout2btn').style.backgroundColor ='#606060';
      document.getElementById('layout1btn').style.backgroundColor ='#E0E0E0';
      document.getElementById('layout3btn').style.backgroundColor ='#E0E0E0';
+     // card two
+    document.getElementById('cardOne').style.display ='none';
+    document.getElementById('cardTwo').style.display ='block';
+    document.getElementById('cardThree').style.display ='none';
 }) 
 
-  document.getElementById('layout3btn').addEventListener('click', function(){
+  const lay3= document.getElementById('layout3btn').addEventListener('click', function(){
     document.getElementById('layout3btn').style.backgroundColor ='#606060';
     document.getElementById('layout2btn').style.backgroundColor ='#E0E0E0';
     document.getElementById('layout1btn').style.backgroundColor ='#E0E0E0';
+    // card three
+    document.getElementById('cardOne').style.display ='none';
+    document.getElementById('cardTwo').style.display ='none';
+    document.getElementById('cardThree').style.display ='block';
   })
 
 
